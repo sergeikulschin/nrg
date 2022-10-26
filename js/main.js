1b2a8aa776978ca8298a9c61b47a7e2a83ca8e88
sergeikulschin <stalkersk2001@mail.ru>
2022-10-27 01:30:35 +0700

    menu

--- js/main.js
+++ js/main.js
@@ -5,4 +5,15 @@ $(function(){
     autoplay:3000,
     dots: true
  })
+
+ $('.header-btn').on('click', function(){
+$('.menu').addClass('active');
+ });
+
+$('.close-btn').on('click', function(){
+$('.menu').removeClass('active')
 });
+
+
+});
+
